109061ffb13461c3632a2648c710da8204b1fc9f
Robério <rroberiosouza@gmail.com>
2024-06-14 23:14:46 -0300

    Adicionando na tela de home o FloatingActionButton

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,6 +10,22 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Container(
+        child: Text("Conteúdo"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+    );
   }
 }
