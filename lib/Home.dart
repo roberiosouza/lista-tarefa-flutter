0fe198e5d29b3486754413d0d1d09789852fa44e
Robério <rroberiosouza@gmail.com>
2024-06-19 18:42:37 -0300

    Implementado o ListView Implementado o showdialog

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,6 +8,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _lista = ["Ir ao mercado", "Estudar", "Fazer exercício"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,11 +17,33 @@ class _HomeState extends State<Home> {
         title: Text("Lista de Tarefas"),
         backgroundColor: Colors.deepPurpleAccent,
       ),
-      body: Container(
-        child: Text("Conteúdo"),
+      body: Expanded(
+        child: ListView.builder(
+            itemCount: _lista.length,
+            itemBuilder: (context, index) {
+              return ListTile(title: Text(_lista[index]));
+            }),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Digite uma tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(labelText: "Digite sua tarefa"),
+                    onChanged: (text) {},
+                  ),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text("Cancelar")),
+                    ElevatedButton(onPressed: () {}, child: Text("Salvar"))
+                  ],
+                );
+              });
+        },
         child: Icon(
           Icons.add,
           color: Colors.white,
